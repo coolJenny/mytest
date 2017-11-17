19ff164d5bb6701838e08112a8e85be79ebd9226
coolJenny <31550194+coolJenny@users.noreply.github.com>
2017-11-17 11:09:14 -0500

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "mytest"
+set :repo_url, "git@github.com:coolJenny/mytest.git"
 
 set :branch, :master
-set :deploy_to, '/home/deploy/urlshortner'
+set :deploy_to, '/home/deploy/mytest'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
